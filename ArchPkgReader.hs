a3f9291dd625355efb83ba2d3c7e4e3e7f391109
Jochen Keil <jochen.keil@gmail.com>
2015-07-06 16:57:19 +0200

    Clean up and turn into proper module

--- ArchPkgReader.hs
+++ ArchPkgReader.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE FlexibleContexts #-}
 
--- module ArchPkgReader where
+module ArchPkgReader where
 
 import System.Environment (getArgs)
 import System.FilePath.Posix ((</>))
@@ -16,86 +16,32 @@ import Control.Applicative
 import Control.Monad.Catch
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Control
-import Control.Monad.Trans.Control
+-- import Control.Monad.Trans.Control
 
-import qualified Data.ByteString as BS
+-- import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BL
-import qualified Data.ByteString.Lazy.Char8 as B8
+-- import qualified Data.ByteString.Lazy.Char8 as B8
 import Data.Conduit
 import Data.Conduit.Lzma
 import qualified Data.Conduit.Combinators as Cmb
 import Codec.Archive.Tar (Entries, FormatError)
 import Control.Monad.Trans.Resource (runResourceT)
-import Control.Monad.Trans.Resource.Internal (MonadResource)
+-- import Control.Monad.Trans.Resource.Internal (MonadResource)
 
-import Codec.Archive.Tar (Entries(..))
+-- import Codec.Archive.Tar (Entries(..))
 import qualified Codec.Archive.Tar as Tar
-import Codec.Archive.Tar.Entry (Entry(..), EntryContent(..))
+import Codec.Archive.Tar.Entry (Entry(..))
 import qualified Codec.Archive.Tar.Entry as Tar
 
-import Debug.Trace
+-- import Debug.Trace
 
--- readPkg :: FilePath -> ByteString
--- readPkg = sourceFile
-
--- tarConduitRead :: Monad m => Conduit BS.ByteString m (Entries FormatError)
--- tarConduitRead = Cmb.mapM (return . strictTarRead)
---     where strictTarRead = Tar.read . BL.fromChunks . (:[])
-
--- pkgEntries :: (MonadBaseControl IO m, MonadThrow m, MonadIO m)
---                => FilePath -> m (Maybe (Entries FormatError))
-
--- pkgEntries :: MonadResource m => FilePath -> ConduitM a (Entries FormatError) m ()
--- pkgEntries fp = Cmb.sourceFile fp $= decompress Nothing $= tarConduitRead
 readLzma :: (MonadBaseControl IO m, MonadIO m, MonadThrow m)
          => FilePath -> m BL.ByteString
-         -- => FilePath -> m BS.ByteString
 readLzma fp = runResourceT $ Cmb.sourceFile fp $= decompress Nothing $$ Cmb.sinkLazy
--- readLzma fp = runResourceT $ Cmb.sourceFile fp $= decompress Nothing $$ fmap BS.concat Cmb.sinkList
-
-readPkgEntries :: (MonadBaseControl IO m, MonadThrow m, MonadIO m)
-               => FilePath -> m (Entries FormatError)
--- readPkgEntries = fmap (Tar.read . BL.fromChunks . (:[])) . readLzma
-readPkgEntries = fmap Tar.read . readLzma
--- readPkgEntries = undefined
-
-    -- Cmb.sourceFile fp $= decompress Nothing $= tarConduitRead $= await
 
 -- readPkgEntries :: (MonadBaseControl IO m, MonadThrow m, MonadIO m)
---                => FilePath -> m (Maybe (Entries FormatError))
--- readPkgEntries fp = runResourceT $ runConduit $ do
---     Cmb.sourceFile fp $= decompress Nothing $= tarConduitRead $= await
-
--- foo fp = runResourceT $ runConduit $ do
---     Cmb.sourceFile fp $= decompress Nothing $= Cmb.sinkFile "/tmp/foo"
-
--- getEntries :: Entries e -> [Tar.TarPath]
--- getEntries = Tar.foldEntries (\e ls -> e : ls) [] (const [])
--- getEntries :: Show e => Entries e -> [Either String Entry]
--- getEntries = Tar.foldEntries (\e ls -> Right e : ls) [] (\e -> [Left $ show e])
-
-toEntryList :: Entries e -> [Entry]
-toEntryList = Tar.foldEntries (:) [] (const [])
-
--- myFoldEntries :: (Entry -> a -> a) -> a -> (e -> a) -> Entries e -> a
--- myFoldEntries f acc err entries = case entries of
---     Done -> acc
---     Fail e -> err e
---     -- Fail e -> myFoldEntries f err err entries
---     Next entry entries' -> myFoldEntries f (f entry acc) err entries'
-
--- showEntry :: Entry -> Maybe String
--- showEntry entry = case Tar.entryContent entry of
---     -- NormalFile n _ -> Just $ B8.unpack n
---     NormalFile n _ -> Just . Tar.fromTarPath . Tar.entryTarPath $ entry
---     Directory      -> Just . Tar.fromTarPath . Tar.entryTarPath $ entry
---     SymbolicLink l -> Just $ Tar.fromLinkTarget l
---     HardLink l     -> Just $ Tar.fromLinkTarget l
---     _              -> Nothing
-
--- showEither :: (e -> String) -> (a -> String) -> Either e a -> String
--- showEither ef _ (Left e)  = ef e
--- showEither _ af (Right a) = af a
+--                => FilePath -> m (Entries FormatError)
+-- readPkgEntries = fmap Tar.read . readLzma
 
 isUnitFile :: Entry -> Bool
 isUnitFile fp = isPrefixOf "usr/lib/systemd" p && isSuffixOf ".service" p
@@ -113,8 +59,8 @@ isGz = isSuffixOf ".gz"
 isLzma :: FilePath -> Bool
 isLzma = isSuffixOf ".xz"
 
-filterLzma :: [FilePath] -> [FilePath]
-filterLzma = filter isLzma
+-- filterLzma :: [FilePath] -> [FilePath]
+-- filterLzma = filter isLzma
 
 maxSize :: Int
 maxSize = 1
@@ -130,8 +76,6 @@ isSmallerThan n = fmap ((n >) . fileSize) . getFileStatus
 --     if b then hasUnitFiles fp
 --          else return b
 
-type Limit = Int
-
 unpack :: FilePath -> IO (Entries FormatError)
 unpack fp = decomp >>= return . Tar.read
     where
@@ -140,32 +84,23 @@ unpack fp = decomp >>= return . Tar.read
            | isLzma fp = readLzma fp
            | otherwise = return ""
 
--- packageContents :: FilePath -> IO (Entries e)
--- findPackage (pkg:pkgs)
---     | isTar pkg  =
---     | isGz pkg   =
---     | isLzma pkg = readPkgEntries pkg
-
 findPackages :: (Entries FormatError -> IO Bool) -> [FilePath] -> IO [FilePath]
--- findPackages f = filterM ((f =<<) . unpack)
-findPackages f = find []
-    -- unpack fp >>= f >>= \b
-
+findPackages f = findpkg []
     where
-    find acc [] = return acc
-    find acc (fp:fps) =
-        unpack fp >>= f >>= \b -> if b then find (fp:acc) fps else find acc fps
-    -- mapM (\fp -> f fp >>= \b -> when b $ return fp)
-
--- filterLzma <$> Tar.getDirectoryContentsRecursive base
---     >>= mapM (\fp -> unitFileCheck fp >>= \b -> when b $ IO.hPutStrLn IO.stderr fp)
+    findpkg r [] = return r
+    findpkg r (fp:fps) =
+        unpack fp >>= f >>= \b -> if b then findpkg (fp:r) fps else findpkg r fps
 
 matchUnitFiles :: Entries FormatError -> IO Bool
 matchUnitFiles = return
                . foldr (||) False
-               -- . map isUnitFile
-               -- . Tar.foldEntries ((:[]) . Tar.entryPath) [] (const [])
                . Tar.foldEntries ((:) . isUnitFile) [] (const [])
+
+findPkgsWithUnitFiles :: FilePath -> IO [FilePath]
+findPkgsWithUnitFiles base = do
+    map (base </>) <$> Tar.getDirectoryContentsRecursive base
+        >>= filterM (isSmallerThan (fromIntegral maxSize * 1024 * 1024))
+        >>= findPackages matchUnitFiles
 
 main :: IO ()
 main = do
@@ -177,17 +112,3 @@ main = do
         >>= filterM (isSmallerThan (fromIntegral maxSize * 1024 * 1024))
         >>= findPackages matchUnitFiles
         >>= mapM_ (IO.hPutStrLn IO.stderr)
-
-    -- (map (showEither id (Tar.fromTarPath . Tar.entryTarPath)) . getEntries)
-    --     <$> readPkgEntries (args !! 0) >>= print
-
-    -- map (base </>) . filterLzma <$> Tar.getDirectoryContentsRecursive base
-    --     -- >>= filterM (isSmallerThan (10 * 1024 * 1024))
-    --     -- >>= filterM unitFileCheck >>= IO.hPutStrLn IO.stderr . unlines
-    --     >>= mapM (\fp -> unitFileCheck fp >>= \b -> when b $ IO.hPutStrLn IO.stderr fp)
-
-        -- >>= mapM_ (\fp -> hasUnitFiles fp >>= \b -> when b $ IO.hPutStrLn IO.stderr fp)
-        -- >>= mapM_ (\fp -> IO.hPutStrLn IO.stderr fp >> readPkgEntries fp)
-        -- >>= filterM (\fp -> IO.hPutStrLn IO.stderr fp >> readPkgEntries fp >> return True)
-
-    return ()
