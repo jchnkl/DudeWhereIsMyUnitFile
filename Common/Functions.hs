2a9c39be534a327c7964982e437226a9dfd73c2f
Jochen Keil <jochen.keil@gmail.com>
2015-07-09 11:57:16 +0200

    Fix unit file check
    Rpm adds files with a leading slash *sigh*

--- Common/Functions.hs
+++ Common/Functions.hs
@@ -1,6 +1,6 @@
 module Common.Functions where
 
-import Data.List (isPrefixOf, isSuffixOf)
+import Data.List (isInfixOf, isSuffixOf)
 import Control.Monad.Trans.Maybe
 
 infixr 5 </>
@@ -11,7 +11,7 @@ hoistMaybeT :: Monad m => Maybe a -> MaybeT m a
 hoistMaybeT = MaybeT . return
 
 isUnitFile :: FilePath -> Bool
-isUnitFile fp = isPrefixOf "usr/lib/systemd" fp && isSuffixOf ".service" fp
+isUnitFile fp = isInfixOf "usr/lib/systemd" fp && isSuffixOf ".service" fp
 
 hasUnitFiles :: [FilePath] -> Bool
 hasUnitFiles = foldr (||) False . map isUnitFile
